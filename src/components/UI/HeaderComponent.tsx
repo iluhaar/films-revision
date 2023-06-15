7d7c89840647fa3cf98ea1f9d7091c87b17a7df8
iluhaar <iluhaar@gmail.com>
2023-06-15 13:23:48 +0300

    add new component to header

--- src/components/UI/HeaderComponent.tsx
+++ src/components/UI/HeaderComponent.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 
+import AddNewFilm from "../AddNewFilm/AddNewFilm";
 import { Notification } from "../Notification/Notification";
 
 const HeaderComponent = () => {
@@ -9,6 +10,8 @@ const HeaderComponent = () => {
         <Link to="/">
           <h1 className="header-1">Films revision</h1>
         </Link>
+        <div style={{ display: "flex", alignItems: "flex-end", justifyContent: "flex-end" }}></div>
+        <AddNewFilm />
       </header>
       <Notification />
     </>
